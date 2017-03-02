bb5fcd8ed86efa47adabd3538f99b5541c2997f1
vinsurf <vinsurf@gmail.com>
2017-03-02 20:08:58 +0100

    Update giorgino-01.sh

--- giorgino-01.sh
+++ giorgino-01.sh
@@ -1,12 +1,19 @@
 #!/bin/bash         
-
-echo "This script install several software for Ubuntooth, useful in digital dentistry workflow, and add some ppa to keep the software updated to latest version"
-echo "Adding ppa...."
+#Adding Patrick Moore repository's
+cd /usr/local/vinsurf
+sudo git clone https://github.com/patmo141/odc_public.wiki.git
+sudo mkdir /usr/local/vinsurf/scripts
+sudo mkdir /usr/local/vinsurf/scripts/addons
+sudo mkdir /usr/local/vinsurf/scripts/modules
+cd /usr/local/vinsurf/scripts/addons
+sudo git clone https://github.com/patmo141/odc_public.git
+sudo git clone https://github.com/patmo141/cut_mesh.git
+#This script install several software for Ubuntooth, useful in digital dentistry workflow, and add some ppa to keep the software updated to latest version
+#Adding ppa....
 sudo add-apt-repository -y ppa:thomas-schiex/blender
 sudo add-apt-repository -y ppa:kritalime/ppa
 sudo add-apt-repository -y ppa:zarquon42/meshlab
-
-echo "Installing Blender, Krita, Meshlab..."
+#Installing Blender, Krita, Meshlab...
 sudo apt update -y && sudo apt install -y blender krita krita-l10n meshlab
 
 
