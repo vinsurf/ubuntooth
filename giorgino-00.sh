ea7f6958f19c31dfb597721a6d9a69d4fb3615b0
vinsurf <vinsurf@gmail.com>
2017-03-03 11:30:11 +0100

    Update giorgino-00.sh

--- giorgino-00.sh
+++ giorgino-00.sh
@@ -1,18 +1,17 @@
 #!bin/bash
-sudo su
 #This script install several software for Ubuntooth, useful in digital dentistry workflow, and add some ppa to keep the software updated to latest version
-apt install -y curl jq 
+sudo apt install -y curl jq 
 cd /usr/local/vinsurf
-mkdir /usr/local/vinsurf/scripts
-mkdir /usr/local/vinsurf/scripts/modules
-mkdir /usr/local/vinsurf/scripts/addons
+sudo mkdir /usr/local/vinsurf/scripts
+sudo mkdir /usr/local/vinsurf/scripts/modules
+sudo mkdir /usr/local/vinsurf/scripts/addons
 cd /usr/local/vinsurf/scripts/addons
 #Adding Patrick Moore repository's
-curl https://api.github.com/users/patmo141/repos | jq .[].clone_url | xargs -n 1 git clone
+sudo curl https://api.github.com/users/patmo141/repos | jq .[].clone_url | xargs -n 1 git clone
 #Adding ppa....
-add-apt-repository -y ppa:thomas-schiex/blender
-add-apt-repository -y ppa:kritalime/ppa
-add-apt-repository -y ppa:zarquon42/meshlab
+sudo add-apt-repository -y ppa:thomas-schiex/blender
+sudo add-apt-repository -y ppa:kritalime/ppa
+sudo add-apt-repository -y ppa:zarquon42/meshlab
 #Installing Blender, Krita, Meshlab...
 sudo apt update -y && sudo apt install -y blender krita krita-l10n meshlab
 
