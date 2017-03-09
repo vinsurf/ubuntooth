e22061c7fcb4e03386b0701ff531652cc6598767
vinsurf <vinsurf@gmail.com>
2017-03-09 11:06:39 +0100

    Update giorgino-test.sh

--- giorgino-test.sh
+++ giorgino-test.sh
@@ -11,13 +11,22 @@ cd /usr/local/vinsurf/scripts/addons
 #Adding Patrick Moore git repository's
 sudo curl https://api.github.com/users/patmo141/repos | jq .[].clone_url | xargs -n 1 git clone
 #Adding ppa....
-sudo add-apt-repository -y ppa:thomas-schiex/blender
-sudo add-apt-repository -y ppa:kritalime/ppa
-sudo add-apt-repository -y ppa:zarquon42/meshlab
-#Installing Blender, Krita, Meshlab...
-sudo apt update -y && sudo apt install -y blender krita krita-l10n meshlab
-sudo dpkg --configure -a
+#sudo add-apt-repository -y ppa:thomas-schiex/blender
+#sudo add-apt-repository -y ppa:kritalime/ppa
+#sudo add-apt-repository -y ppa:zarquon42/meshlab
+#Installing Software
+sudo apt update -y 
+#sudo apt install -y blender krita krita-l10n meshlab
+#sudo dpkg --configure -a
 #Give permission to write in folder vinsurf
+sudo snap install blender
+sudo snap install meshlab
+sudo snap install cloudcompare
+sudo snap install krita
+sudo snap install inkscape
+#sudo snap install libreoffice
+#sudo snap install telegram
+#sudo snap install simplescreenrecorder-mardy
 cd /usr/local
 sudo chmod -R 777 vinsurf
 
